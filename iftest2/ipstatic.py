623118db3ea0da3c86dd421f9537932ca5556144
liercke <liercke@gmail.com>
2020-09-15 21:38:53 +0000

    Starting with elif

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+ipchk = input("Apply an IP address: ")
+
+if ipchk == "192.168.70.1":
+    print("Looks like the IP address of the gateway was set: " + ipchk + " This is not recommneded.")
+elif ipchk:
+    print("Looks like the IP address was set:" + ipchk)
+else:
+    print("You did not enter an IP address.")
+
+
