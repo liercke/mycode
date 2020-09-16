4b89cbf2462fe1c4fff8f33315ffe8fa77b3f0e4
liercke <liercke@gmail.com>
2020-09-16 19:28:57 +0000

    Parsing Log Files

---
+++ attemptlogin/keystone_counter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+loginfail = 0
+
+keystone_file = open("/home/student/mycode/attemptlogin/keystone.common.wsgi", "r")
+
+keystone_file_lines=keystone_file.readlines()
+
+for line in keystone_file_lines:
+    if "- - - - -] Authorization failed" in line:
+        loginfail += 1
+print("The number of failed login attempts is", loginfail)
+keystone_file.close()
---
+++ attemptlogin/large_fileparse-with.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+loginfail = 0
+
+with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
+
+    for line in kfile:
+        if "- - - - -] Authorization failed" in line:
+            loginfail += 1
+            print("Failed login from: " + line.split(" ")[-1])
+print("The number of failed logins is", loginfail)
+
