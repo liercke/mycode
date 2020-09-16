4dbeaa66fe75049267bd0a94de664f51e442654b
liercke <liercke@gmail.com>
2020-09-16 17:38:29 +0000

    Added some UUID's

---
+++ fact/looping03.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+
+import uuid
+
+howmany = int(input("How many UUIDs should be generated?"))
+
+print("Generating UUIDs...")
+
+for rando in range(howmany):
+    print(uuid.uuid4())
+
